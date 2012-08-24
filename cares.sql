98e6e6bd2496d7c611bcabbe37ef7d3c10040cb8
Mark Paschal <markpasc@markpasc.org>
2012-08-24 13:59:50 -0400

    Oh hey here's an app

---
+++ cares.sql
@@ -0,0 +1,11 @@
+CREATE TABLE post (
+	id SERIAL,
+	html VARCHAR(1024),
+	posted TIMESTAMP
+);
+
+CREATE TABLE follower (
+);
+
+CREATE TABLE follow (
+);
---
+++ main.go
@@ -0,0 +1,236 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"encoding/base64"
+	"time"
+	"database/sql"
+	"strconv"
+	_ "github.com/jbarham/gopgsqldriver"
+	"github.com/hoisie/mustache"
+)
+
+var db *sql.DB
+
+type Post struct {
+	Id int
+	Html string
+	Posted time.Time
+}
+
+func NewPost() (p *Post) {
+	p = &Post{0, "", time.Now()}
+	return
+}
+
+func (p *Post) Permalink() string {
+	return fmt.Sprintf("/%d/%d", p.Posted.Year(), p.Id)
+}
+
+func (p *Post) PostedTime() string {
+	return p.Posted.Format("3:04")
+}
+
+func (p *Post) PostedAM() string {
+	return p.Posted.Format("PM")
+}
+
+func (p *Post) PostedDate() string {
+	return p.Posted.Format("_2 Jan 2006")
+}
+
+func (p *Post) Save() (err error) {
+	if p.Id == 0 {
+		//var result sql.Result
+		//result, err = db.Exec("INSERT INTO post (html, posted) VALUES ($1, $2) RETURNING id",
+		row := db.QueryRow("INSERT INTO post (html, posted) VALUES ($1, $2) RETURNING id",
+			p.Html, p.Posted)
+		var id int
+		err = row.Scan(&id)
+		if (err != nil) {
+			return err
+		}
+		p.Id = id
+	} else {
+		_, err = db.Exec("UPDATE post SET html = $1, posted = $2 WHERE id = $3",
+			p.Html, p.Posted, p.Id)
+	}
+	return nil
+}
+
+func PostById(id int) (*Post, error) {
+	row := db.QueryRow("SELECT html, posted FROM post WHERE id = $1", id)
+
+	var html string
+	var posted string
+	err := row.Scan(&html, &posted)
+	if (err != nil) {
+		log.Println("Error querying database for post #", id, ":", err.Error())
+		return nil, err
+	}
+	log.Println("Got time string:", posted)
+	postedTime, err := time.Parse("2006-01-02 15:04:05.000000", posted)
+	if (err != nil) {
+		log.Println("Error converting database date", posted, "to a time:", err.Error())
+		return nil, err
+	}
+
+	post := &Post{id, html, postedTime}
+	return post, nil
+}
+
+func RecentPosts(count int) ([]*Post, error) {
+	rows, err := db.Query("SELECT * FROM post ORDER BY posted DESC LIMIT 10")
+	if (err != nil) {
+		log.Println("Error querying database for", count, "posts:", err.Error())
+		return nil, err
+	}
+
+	log.Println("Deserializing all the returned posts")
+	posts := make([]*Post, 0, count)
+	var id int
+	var html string
+	var posted string
+	var postedTime time.Time
+	i := 0
+	for rows.Next() {
+		err = rows.Scan(&id, &html, &posted)
+		if (err != nil) {
+			log.Println("Error scanning row", i, ":", err.Error())
+			return nil, err
+		}
+
+		log.Println("Got time string", posted)
+		postedTime, err = time.Parse("2006-01-02 15:04:05.000000", posted)
+		posts = posts[0:i+1]
+		posts[i] = &Post{id, html, postedTime}
+		i++
+	}
+
+	err = rows.Err()
+	if (err != nil) {
+		log.Println("Error lookin' at rows:", err.Error())
+		return nil, err
+	}
+
+	return posts, nil
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	posts, err := RecentPosts(10)
+	if (err != nil) {
+		log.Println("OOPS ERROR", err.Error())
+	} else {
+		log.Println("OHAI", len(posts), "posts")
+	}
+
+	data := make(map[string] interface{})
+	data["posts"] = posts
+	fmt.Fprintf(w, mustache.RenderFile("index.html", data))
+}
+
+func permalink(w http.ResponseWriter, r *http.Request) {
+	idstr := r.URL.Path[len("/post/"):]
+	log.Println("Finding id from path component", idstr)
+	id, err := strconv.ParseInt(idstr, 10, 32)
+	if (err != nil) {
+		http.Error(w, "invalid post id: " + err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var post *Post
+	post, err = PostById(int(id))
+	if (err != nil) {
+		http.Error(w, fmt.Sprintf("invalid post #%d: %s", id, err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	data := make(map[string] interface{})
+	data["post"] = post
+	fmt.Fprintf(w, mustache.RenderFile("permalink.html", data))
+}
+
+func isAuthed(w http.ResponseWriter, r *http.Request) (authed bool) {
+	authed = false
+	authHeader := r.Header.Get("Authorization")
+	if strings.HasPrefix(authHeader, "Basic ") {
+		log.Println("Yay, client gave a Basic auth header")
+		userpass, err := base64.StdEncoding.DecodeString(authHeader[6:])
+		if err == nil {
+			userpassParts := strings.SplitN(string(userpass), ":", 2)
+			user, pass := userpassParts[0], userpassParts[1]
+			if user == "markpasc" && pass == "password" {
+				log.Println("Yay, client authorized!")
+				authed = true
+			} else {
+				log.Println("Oops, client gave a bad username:password pair")
+			}
+		} else {
+			log.Println("Oops, error decoding the client's Basic auth header:", err.Error())
+		}
+	}
+
+	if !authed {
+		w.Header().Set("WWW-Authenticate", "Basic realm=\"cares\"")
+		http.Error(w, "authorization required", http.StatusUnauthorized)
+	}
+	return
+}
+
+func makepost(w http.ResponseWriter, r *http.Request) {
+	if !isAuthed(w, r) {
+		return
+	}
+
+	// Is this a POST? Make a new post then!
+	if r.Method == "POST" {
+		post := NewPost()
+		html := r.FormValue("html")
+		if html == "" {
+			http.Error(w, "html value is required", http.StatusBadRequest)
+		}
+
+		post.Html = html
+		err := post.Save()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		http.Redirect(w, r, post.Permalink(), http.StatusTemporaryRedirect)
+		return
+	}
+
+	data := make(map[string] interface{})
+	fmt.Fprintf(w, mustache.RenderFile("makepost.html", data))
+}
+
+func static(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path[1:]
+	log.Println("Serving static file", path)
+	http.ServeFile(w, r, path)
+}
+
+func main() {
+	var err error
+	db, err = sql.Open("postgres", "host=localhost dbname=cares")
+	if err == nil {
+		// Try a query to make sure it worked.
+		_, err = db.Query("SELECT 1")
+	}
+	if err != nil {
+		log.Println("Error connecting to db:", err.Error())
+		return
+	}
+
+	http.HandleFunc("/static/", static)
+	http.HandleFunc("/2012/", permalink)
+	http.HandleFunc("/post", makepost)
+	http.HandleFunc("/", index)
+
+	log.Println("Ohai web servin'")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
