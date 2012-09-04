d856cc99e25bcc7277521637de537b2c23ce654f
Mark Paschal <markpasc@markpasc.org>
2012-09-04 13:53:32 -0400

    Add --init-db command for setting up database

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"database/sql"
 	"flag"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -27,6 +28,31 @@ func Prompt(prompt string) (ret string) {
 	return
 }
 
+func InitializeDatabase() {
+	schemaBytes, err := ioutil.ReadFile("cares.sql")
+	if err != nil {
+		log.Println("Error reading database schema:", err.Error())
+		return
+	}
+
+	statements := strings.Split(string(schemaBytes), ";\n")
+	for _, statement := range statements {
+		statement = strings.TrimSpace(statement)
+		if statement == "" {
+			continue
+		}
+
+		_, err = db.Exec(statement)
+		if err != nil {
+			log.Println("Error initializing database:", err.Error())
+			return
+		}
+	}
+
+	// Then make the owner record too.
+	MakeAccount()
+}
+
 func MakeAccount() {
 	name := Prompt("Name: ")
 	pass := Prompt("Password: ")
@@ -43,6 +69,12 @@ func MakeAccount() {
 }
 
 func ServeWeb() {
+	err := LoadAccountForOwner()
+	if err != nil {
+		log.Println("Error loading site owner:", err.Error())
+		return
+	}
+
 	http.HandleFunc("/static/", static)
 	http.HandleFunc("/rss", rss)
 	http.HandleFunc("/rssCloud", rssCloud)
@@ -56,12 +88,16 @@ func ServeWeb() {
 }
 
 func main() {
+	var dsn string
 	var makeaccount bool
+	var initdb bool
+	flag.StringVar(&dsn, "database", "dbname=cares sslmode=disable", "database connection info")
+	flag.BoolVar(&initdb, "init-db", false, "initialize the database")
 	flag.BoolVar(&makeaccount, "make-account", false, "create a new account interactively")
 	flag.Parse()
 
 	var err error
-	db, err = sql.Open("postgres", "host=localhost dbname=cares sslmode=disable")
+	db, err = sql.Open("postgres", dsn)
 	if err == nil {
 		// Try a query to make sure it worked.
 		_, err = db.Query("SELECT 1")
@@ -70,13 +106,10 @@ func main() {
 		log.Println("Error connecting to db:", err.Error())
 		return
 	}
-	err = LoadAccountForOwner()
-	if err != nil {
-		log.Println("Error loading site owner:", err.Error())
-		return
-	}
 
-	if makeaccount {
+	if initdb {
+		InitializeDatabase()
+	} else if makeaccount {
 		MakeAccount()
 	} else {
 		ServeWeb()
